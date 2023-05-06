9d0ac73e2f0f6a692b706f17dae1d3c32ffaa08c
Rahman <101407836+Rahman1108@users.noreply.github.com>
2023-05-06 15:10:09 +0530

    Create Stdin_Stdout.java

---
+++ Stdin_Stdout.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int myInt1 = scan.nextInt();
+        int myInt2 = scan.nextInt();
+        int myInt3 = scan.nextInt();
+        System.out.println(myInt1+"\n"+myInt2+"\n"+myInt3);
+        
+    }
+}
+
+// input : 
+// Hi 5
+
+// output : 
+// myString is: Hi
+//myInt is: 5
