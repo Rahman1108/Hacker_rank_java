437c153cad6b64fe31912e43321c323eb5b0d8a7
Rahman <101407836+Rahman1108@users.noreply.github.com>
2023-05-06 15:38:07 +0530

    Create int_to_str.java

---
+++ int_to_str.java
@@ -0,0 +1,9 @@
+public class IntToString{  
+  public static void main(String args[]) {  
+  int i=200;  
+    // code of the program below one line .
+  String s=String.valueOf(i);  
+  System.out.println(i);  
+  System.out.println(s);  
+  }
+}  
